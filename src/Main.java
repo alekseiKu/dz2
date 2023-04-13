9f5c7ba569eb7ac4545eeb2dcad8d1c65f544bcf
Kulichkin <kulichkin@savinger.ru>
2023-04-13 11:16:10 +0300

    Added code

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,9 @@
 public class Main {
+    public static void main(String[] args) {
+        int ticketCost = 13676;
+        int miliCost = 20;
+        int mili = ticketCost / miliCost;
+
+        System.out.println("Количество начисленных миль за покупку билета: " + mili);
+    }
 }
